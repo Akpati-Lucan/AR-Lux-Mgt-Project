f6f93ed4e8c6086cea9a2decadbf8f66dd465150
jbs671 <jbs671@mail.usask.ca>
2026-02-10 15:04:57 -0600

    all links work properly

--- frontend/my-app/app/components/footer.tsx
+++ frontend/my-app/app/components/footer.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 const footer = () => {
   return (
     <div className='p-2 h-20 bg-black flex items-center justify-between'>
-        <p>© 2026 A R LUXURY MANAGEMENT INC. ALL RIGHTS RESERVED</p>
+        <p className='text-white p-2'>© 2026 A R LUXURY MANAGEMENT INC. ALL RIGHTS RESERVED</p>
 
-        <div className='flex gap-4 text-purple-600 text-2xl'>
-            <Link className="link" href="/privacy-policy">PRIVACY POLICY</Link>
-            <Link className="link" href="/terms-and-conditions">TERMS AND CONDITIONS</Link>
-            <Link className="link" href="/careers">CAREERS</Link>
+        <div className='flex gap-4 text-gray-500 text-2xl'>
+            <Link href="/privacy-policy">PRIVACY POLICY</Link>
+            <Link href="/terms-and-conditions">TERMS AND CONDITIONS</Link>
+            <Link href="/careers">CAREERS</Link>
         </div>
     </div>
   )
--- frontend/my-app/app/page.tsx
+++ frontend/my-app/app/page.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
-import Header from "./components/Header";
-import Footer from "./components/footer"
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <p>Hello world</p>
-      <Footer />
     </main>
   );
 }

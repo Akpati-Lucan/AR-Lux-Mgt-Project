54341740d7fb5ced4c539fff32289b14f999b62d
jbs671 <jbs671@mail.usask.ca>
2026-02-10 14:40:34 -0600

    added footer component

--- frontend/my-app/app/components/footer.tsx
+++ frontend/my-app/app/components/footer.tsx
@@ -3,7 +3,15 @@ import Link from 'next/link'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <div className='p-2 h-20 bg-black flex items-center justify-between'>
+        <p>© 2026 A R LUXURY MANAGEMENT INC. ALL RIGHTS RESERVED</p>
+
+        <div className='flex gap-4 text-purple-600 text-2xl'>
+            <Link className="link" href="/About">PRIVACY POLICY</Link>
+            <Link className="link" href="/Expertise">TERMS AND CONDITIONS</Link>
+            <Link className="link" href="/Network">CAREERS</Link>
+        </div>
+    </div>
   )
 }
 
--- frontend/my-app/app/components/header.tsx
+++ frontend/my-app/app/components/header.tsx
@@ -4,18 +4,18 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <div className='p-2 bg-purple-400 flex items-center justify-between'>
+    <div className='p-2 h-20 bg-white flex items-center justify-between'>
         <Link className="link" href="/">AR Logo</Link>
 
-        <div className='flex gap-4'>
+        <div className='flex gap-4 text-purple-600 text-2xl'>
             <Link className="link" href="/About">About</Link>
             <Link className="link" href="/Expertise">Expertise</Link>
             <Link className="link" href="/Network">Network</Link>
             <Link className="link" href="/Contact">Contact</Link>
-            <Link className="link" href="/Signin">Signin</Link>
+            <Link className="link" href="/Signin">Sign In</Link>
         </div>
     </div>
-
+    <div className='h-1 bg-purple-600 flex'></div>
     </>
   )
 }

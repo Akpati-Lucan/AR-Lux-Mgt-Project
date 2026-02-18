15a8510a0e78639f0ff763140ea72214dfcac71c
jbs671 <jbs671@mail.usask.ca>
2026-02-17 23:24:47 -0600

    fixed overlay error

--- frontend/my-app/app/home/DesktopHomePage.tsx
+++ frontend/my-app/app/home/DesktopHomePage.tsx
@@ -2,7 +2,7 @@ import BackgroundCollage from './BackgroundCollage/BackgroundCollage'
 
 const DesktopHomePage = () => {
   return (
-    <div>
+    <div className="relative min-h-screen flex flex-col">
       <BackgroundCollage />
       <div className="flex items-center justify-center h-screen absolute inset-0">
         <p className="text-white text-6xl text-center">
--- frontend/my-app/app/home/MobileHomePage.tsx
+++ frontend/my-app/app/home/MobileHomePage.tsx
@@ -2,7 +2,7 @@ import BackgroundCollage from './BackgroundCollage/BackgroundCollage'
 
 const MobileHomePage = () => {
   return (
-    <div>
+    <div className="relative min-h-screen flex flex-col">
       <BackgroundCollage />
       <div className="flex items-center justify-center h-screen absolute inset-0">
         <p className="text-white text-3xl text-center">

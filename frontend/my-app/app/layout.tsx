109fc5f2e1dcf6bef9566ab78d86473afcbb970d
jbs671 <jbs671@mail.usask.ca>
2026-02-27 17:09:43 -0600

    added suppressed hydration heading to layout.tsx

--- frontend/my-app/app/layout.tsx
+++ frontend/my-app/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className='min-h-screen flex flex-col'>
         <Header />
             <main className='grow'>{children}</main>

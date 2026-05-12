a17f31bbb9ab916ff15ddca3729b13db367de015
jbs671 <jbs671@mail.usask.ca>
2026-05-11 22:58:06 -0600

    modified header

--- frontend/my-app/app/components/header/DesktopHeader.tsx
+++ frontend/my-app/app/components/header/DesktopHeader.tsx
@@ -5,15 +5,15 @@ const Header = () => {
 
   return (
     <>
-      <div className='h-21 bg-gray-900 flex gap-4 items-center justify-between'>
-        <div className="bg-linear-to-r from-purple-200 via-purple-100 to-white p-2  
+      <div className='h-21 bg-gray-950 text-gray-400 flex gap-4 items-center justify-between'>
+        <div className="bg-linear-to-r from-gray-400 via-gray-200 to-white p-2  
                         flex items-center justify-center h-21 w-55 ">
           <Link href="/">
             <Image src={"/AR-lux-mgmt-logo.png"} alt="AR Logo" width={120} height={40} priority />
           </Link>
         </div>
 
-        <div className='flex gap-4 text-purple-400 text-lg p-4'>
+        <div className='flex gap-4 text-gray-400 text-lg p-4'>
             <Link href="/about" className="hover:text-white">About</Link>
             <Link href="/expertise" className="hover:text-white">Expertise</Link>
             <Link href="/network" className="hover:text-white">Network</Link>
@@ -22,7 +22,7 @@ const Header = () => {
             <Link href="/signin" className="hover:text-white">Sign In</Link>
         </div>
       </div>
-      <div className='h-1.5 bg-purple-600 flex'></div>
+      <div className='h-1.5 bg-gray-950 flex'></div>
     </>
   )
 }
